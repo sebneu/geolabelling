ff26f794772b65d5d7fa4dec3d1348d6d7d05096
neumaier <sebastian.neumaier@wu.ac.at>
2017-06-26 17:37:05 +0200

    requirements + setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+from pip.req import parse_requirements
+
+install_reqs = parse_requirements("requirements.txt", session=False)
+
+# reqs is a list of requirement
+# e.g. ['django==1.5.1', 'mezzanine==1.4.6']
+reqs = [str(ir.req) for ir in install_reqs]
+
+setup(
+    name='geo_tagger',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=reqs,
+    license='GNU General Public License v3.0',
+    author='Sebastian Neumaier',
+    author_email='sebastian.neumaier@wu.ac.at',
+    description='A service to add geographical identification metadata to CSV tables.'
+)
